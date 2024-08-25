d0135009ee893b02f25f473290d902af75b99c11
miloskec <milos.kecman@gmail.com>
2024-08-25 18:59:58 +0200

    Update clone-all-set-kubernetes-branch.sh

--- clone-all-set-kubernetes-branch.sh
+++ clone-all-set-kubernetes-branch.sh
@@ -2,36 +2,26 @@
 echo "This script is going to clone and  all necessary repositories for the Laravel Microservice Example."
 set -e
 
-echo "Cloning Kafka..."
-git clone git@github.com:miloskec/kafka.git
-cd kafka
-git checkout kubernetes-example
-cp .env.example .env
-cd -
-echo "Cloning Gateway..."
-git clone git@github.com:miloskec/gateway.git
-cd gateway
-git checkout kubernetes-example
-cp .env.example .env
-cd -
-echo "Cloning Authentication..."
-git clone git@github.com:miloskec/authentication.git
-cd authentication
-git checkout kubernetes-example
-cp .env.example .env
-cd -
-echo "Cloning Authorization..."
-git clone git@github.com:miloskec/authorization.git
-cd authorization
-git checkout kubernetes-example
-cp .env.example .env
-cd -
-echo "Cloning Profile..."
-git clone git@github.com:miloskec/profile.git
-cd profile
-git checkout kubernetes-example
-cp .env.example .env
-cd -
+#!/bin/bash
+
+# Default branch name
+branch_name="${1:-kubernetes-example}"
+
+echo "This script is going to clone and set up all necessary repositories for the Laravel Microservice Example."
+set -e
+
+repos=("kafka" "gateway" "authentication" "authorization" "profile")
+
+for repo in "${repos[@]}"
+do
+    echo "Cloning ${repo}..."
+    git clone git@github.com:miloskec/${repo}.git
+    cd ${repo}
+    git checkout $branch_name
+    cp .env.example .env
+    cd -
+done
+
 echo "Cloning Datadog..."
 git clone git@github.com:miloskec/datadog.git
 cd datadog
